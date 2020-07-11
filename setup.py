0ae1b19107f45bd4748e780c3d7681144a2e6e5e
atreadw1492 <atreadway@markelcorp.com>
2020-07-11 10:05:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
 
 setup(name='yahoo_fin',
-      version='0.8.4',
-      description="""Download historical stock prices (daily / weekly / monthly), 
-                    fundamentals data, income statements, cash flows, 
-                    analyst info, current cryptocurrency prices, option chains,
-                    and more with yahoo_fin.
+      version='0.8.6',
+      description="""Download historical stock prices (daily / weekly / monthly),
+                     realtime-prices, fundamentals data, income statements, 
+                     cash flows, analyst info, current cryptocurrency prices,
+                     option chains, and more with yahoo_fin.
                     """,
       url='http://theautomatic.net/yahoo_fin-documentation/',
       author='Andrew Treadway',
       author_email='opensourcecoder11@gmail.com',
-      license='',
+      license='MIT',
       packages=['yahoo_fin'],
+      keywords = ["yahoo finance", "stocks", "options", "fundamentals"],
 zip_safe=False)
