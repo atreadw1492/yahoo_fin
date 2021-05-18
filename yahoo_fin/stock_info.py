5ebf31b5f53fbec20f67c9d651f9e51313dc25ec
Daniel Catlin <chikin1993@hotmail.co.uk>
2021-05-18 20:57:48 +0100

    Update stock_info.py

--- yahoo_fin/stock_info.py
+++ yahoo_fin/stock_info.py
@@ -969,3 +969,32 @@ def get_postmarket_price(ticker):
     
     raise AssertionError("Postmarket price not currently available.")
     
+
+# Company Information Functions
+def get_company_info(ticker):
+    '''Scrape the company information for a ticker
+
+       @param: ticker
+    '''
+    site = f"https://finance.yahoo.com/quote/{ticker}/profile?p={ticker}"
+    json_info = _parse_json(site)
+    json_info = json_info["assetProfile"]
+    info_frame = pd.DataFrame.from_dict(json_info,
+                                        orient="index",
+                                        columns=["Value"])
+    info_frame = info_frame.drop("companyOfficers", axis="index")
+    info_frame.index.name = "Breakdown"
+    return info_frame
+
+
+def get_company_officers(ticker):
+    '''Scrape the company information and return a table of the officers
+
+       @param: ticker
+    '''
+    site = f"https://finance.yahoo.com/quote/{ticker}/profile?p={ticker}"
+    json_info = _parse_json(site)
+    json_info = json_info["assetProfile"]["companyOfficers"]
+    info_frame = pd.DataFrame.from_dict(json_info)
+    info_frame = info_frame.set_index("name")
+    return info_frame
